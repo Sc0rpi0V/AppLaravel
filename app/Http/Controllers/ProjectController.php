355d44bf1cb1cbabe60adfbc7ebd9dd9f6a7e6a7
valentin <valentinc.boury@gmail.com>
2024-08-05 17:32:21 +0200

    ajout des différents projets et categories + listes

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -47,6 +47,11 @@ class ProjectController extends Controller
             'description' => 'Réalisation de Bloc React Gutenberg / ACF Gutenberg',
             'category' => 'wordpress'
         ],
+        'tma-eldvarm' => [
+            'title' => 'TMA ELDVARM',
+            'description' => 'Réalisation de la maintenance applicative du site...',
+            'category' => 'maintenance'
+        ],
     ];
 
     /**
@@ -78,58 +83,112 @@ class ProjectController extends Controller
         $projects = [
             'tma-ifs' => [
                 'name' => 'TMA IFS',
-                'description' => 'Réalisation de la maintenance applicative du site :
-                    Comprend l\'ensemble des activités de gestion et de maintenance continue d\'un site web basé sur WordPress.
-                    Coeur WordPress : Mise à jour et maintenance de la version principale de WordPress pour assurer la sécurité et la stabilité du site.
-                    Thèmes : Gestion, mise à jour et personnalisation des thèmes WordPress pour répondre aux besoins esthétiques et fonctionnels du site.
-                    Plugins : Installation, mise à jour et configuration des plugins WordPress pour ajouter des fonctionnalités au site.
-                    Contributions : Participation à la création et à la gestion de contenu sur les pages et les articles du site.
-                    Tâches spécifiques réalisées :
-                    - Contribution sur les pages et articles WordPress :
-                    - Rédaction, édition et mise en forme du contenu des différentes pages et articles du site.
-                    - Ajout d\'éléments multimédia (images, vidéos) et liens pour enrichir le contenu.
-                    - Optimisation SEO (Search Engine Optimization) des pages et articles pour améliorer leur visibilité sur les moteurs de recherche.
-                    - Mise à jour des plugins :
-                    - Identification des plugins nécessitant une mise à jour.
-                    - Réalisation des mises à jour en suivant les procédures appropriées pour éviter toute interruption de service.
-                    - Rédaction d\'un listing complet et détaillé des plugins mis à jour, incluant les versions et les dates de mise à jour.
-                    - Tests post-mise à jour pour s\'assurer que les plugins fonctionnent correctement et qu\'il n\'y a pas de conflits.
-                    - Mise à jour des thèmes :
-                    - Mise à jour des thèmes utilisés par le site pour bénéficier des nouvelles fonctionnalités et corrections de sécurité.
-                    - Adaptation des thèmes après mise à jour pour maintenir la cohérence visuelle et fonctionnelle du site.
-                    - Tests pour vérifier que les modifications apportées par les mises à jour sont bien intégrées sans provoquer d\'erreurs.
-                    - Correction des erreurs sur les fichiers thèmes PHP :
-                    - Identification et diagnostic des erreurs dans les fichiers PHP des thèmes.
-                    - Correction des erreurs pour améliorer la fonctionnalité et la performance du site.
-                    - Vérification des modifications pour assurer la compatibilité avec le reste du site et éviter toute régression.',            
+                'subtitle' => 'Institut Français du Sang (Singapour)',
+                'description' => 'Réalisation de la maintenance applicative du site :',
+                'desc-list' => [
+                    'Comprend l\'ensemble des activités de gestion et de maintenance continue d\'un site web basé sur WordPress.',
+                    'Coeur WordPress : Mise à jour et maintenance de la version principale de WordPress pour assurer la sécurité et la stabilité du site.',
+                    'Thèmes : Gestion, mise à jour et personnalisation des thèmes WordPress pour répondre aux besoins esthétiques et fonctionnels du site.',
+                    'Plugins : Installation, mise à jour et configuration des plugins WordPress pour ajouter des fonctionnalités au site.',
+                    'Contributions : Participation à la création et à la gestion de contenu sur les pages et les articles du site.'],
+                'role'  => 'Développeur FullStack',
+                'liste' => [
+                    'Contribution sur les pages et articles WordPress.',
+                    'Rédaction, édition et mise en forme du contenu des différentes pages et articles du site.',
+                    'Ajout d\'éléments multimédia (images, vidéos) et liens pour enrichir le contenu.',
+                    'Optimisation SEO (Search Engine Optimization) des pages et articles pour améliorer leur visibilité sur les moteurs de recherche.',
+                    'Identification des plugins nécessitant une mise à jour.',
+                    'Réalisation des mises à jour en suivant les procédures appropriées pour éviter toute interruption de service.',
+                    'Rédaction d\'un listing complet et détaillé des plugins mis à jour, incluant les versions et les dates de mise à jour.',
+                    'Tests post-mise à jour pour s\'assurer que les plugins fonctionnent correctement et qu\'il n\'y a pas de conflits.',
+                    'Mise à jour des thèmes utilisés par le site pour bénéficier des nouvelles fonctionnalités et corrections de sécurité.',
+                    'Adaptation des thèmes après mise à jour pour maintenir la cohérence visuelle et fonctionnelle du site.',
+                    'Tests pour vérifier que les modifications apportées par les mises à jour sont bien intégrées sans provoquer d\'erreurs.',
+                    'Identification et diagnostic des erreurs dans les fichiers PHP des thèmes.',
+                    'Correction des erreurs pour améliorer la fonctionnalité et la performance du site.',
+                    'Vérification des modifications pour assurer la compatibilité avec le reste du site et éviter toute régression.'
+                ],
+                'category' => 'Maintenance' ,
+                'image' => asset('img/ifs.jpeg'),        
             ],
             'calf-mvp' => [
                 'name' => 'CALF MVP',
-                'description' => 'Réalisation de simulateur pour simulation sa consommation d\'énergie (Particulier / Professionnel)'
+                'subtitle' => 'Crédit Agricole Leasing & Factoring (Simulateurs Autoconsommation Solaire)',
+                'description' => 'Réalisation de simulateur pour simulation sa consommation d\'énergie (Particulier / Professionnel)',
+                'image' => asset('img/calf.png'), 
+                'category' => 'Maintenance' ,
             ],
             'edf-enr' => [
                 'name' => 'EDF-ENR',
-                'description' => 'Réalisation de Bloc React Gutenberg pour l\'ensemble du site (Refonte Complète)'
+                'subtitle' => 'Electricité de France Energies Nouvelles Réparties',
+                'description' => 'Réalisation de Bloc React Gutenberg pour l\'ensemble du site (Refonte Complète)',
+                'image' => asset('img/edfenr.webp'), 
+                'category' => 'Maintenance' ,
             ],
             'engie' => [
                 'name' => 'ENGIE',
-                'description' => 'Réalisation de Bloc React Gutenberg et Gestion d\'une map pour borne de recharge voiture électrique'
+                'subtitle' => 'ENGIE',
+                'description' => 'Réalisation de Bloc React Gutenberg et Gestion d\'une map pour borne de recharge voiture électrique',
+                'image' => asset('img/engie.jpeg'), 
+                'category' => 'Maintenance' ,
             ],
             'thekdo' => [
                 'name' => 'THEKDO',
-                'description' => 'Gestion de la partie Woocommerce et préparation d\'extract de commande avec requête SQL'
+                'subtitle' => 'Crédit Agricole Leasing & Factoring (Simulateurs Autoconsommation Solaire)',
+                'description' => 'Gestion de la partie Woocommerce et préparation d\'extract de commande avec requête SQL',
+                'image' => asset('img/thekdo.webp'), 
+                'category' => 'Maintenance' ,
             ],
             'theme-wordpress' => [
                 'name' => 'THEME WORDPRESS',
-                'description' => 'Création de thème WordPress'
+                'subtitle' => 'Création Thèmes WordPress sur Mesure ou Modification',
+                'description' => 'Création de thème WordPress',
+                'image' => asset('img/ifs.jpeg'), 
+                'category' => 'Maintenance' ,
             ],
             'plugins-wordpress' => [
                 'name' => 'PLUGINS WORDPRESS',
-                'description' => 'Création de plugins WordPress'
+                'subtitle' => 'Plugins de Sécurité, Plugins Customisable',
+                'description' => 'Création de plugins WordPress',
+                'image' => asset('img/ifs.jpeg'), 
+                'category' => 'Maintenance' ,
             ],
             'blocs-wordpress' => [
                 'name' => 'BLOCS WORDPRESS',
-                'description' => 'Réalisation de Bloc React Gutenberg / ACF Gutenberg'
+                'subtitle' => 'Blocs React Gutenberg & Gutenberg ACF',
+                'description' => 'Réalisation de Bloc React Gutenberg / ACF Gutenberg',
+                'image' => asset('img/ifs.jpeg'), 
+                'category' => 'Maintenance' ,
+            ],
+            'tma-eldvarm' => [
+                'name' => 'TMA ELDVARM',
+                'subtitle' => 'ELDVARM',
+                'description' => 'Réalisation de la maintenance applicative du site :',
+                'desc-list' => [
+                    'Comprend l\'ensemble des activités de gestion et de maintenance continue d\'un site web basé sur WordPress.',
+                    'Coeur WordPress : Mise à jour et maintenance de la version principale de WordPress pour assurer la sécurité et la stabilité du site.',
+                    'Thèmes : Gestion, mise à jour et personnalisation des thèmes WordPress pour répondre aux besoins esthétiques et fonctionnels du site.',
+                    'Plugins : Installation, mise à jour et configuration des plugins WordPress pour ajouter des fonctionnalités au site.',
+                    'Contributions : Participation à la création et à la gestion de contenu sur les pages et les articles du site.'],
+                'role'  => 'Développeur FullStack',
+                'liste' => [
+                    'Contribution sur les pages et articles WordPress.',
+                    'Rédaction, édition et mise en forme du contenu des différentes pages et articles du site.',
+                    'Ajout d\'éléments multimédia (images, vidéos) et liens pour enrichir le contenu.',
+                    'Optimisation SEO (Search Engine Optimization) des pages et articles pour améliorer leur visibilité sur les moteurs de recherche.',
+                    'Identification des plugins nécessitant une mise à jour.',
+                    'Réalisation des mises à jour en suivant les procédures appropriées pour éviter toute interruption de service.',
+                    'Rédaction d\'un listing complet et détaillé des plugins mis à jour, incluant les versions et les dates de mise à jour.',
+                    'Tests post-mise à jour pour s\'assurer que les plugins fonctionnent correctement et qu\'il n\'y a pas de conflits.',
+                    'Mise à jour des thèmes utilisés par le site pour bénéficier des nouvelles fonctionnalités et corrections de sécurité.',
+                    'Adaptation des thèmes après mise à jour pour maintenir la cohérence visuelle et fonctionnelle du site.',
+                    'Tests pour vérifier que les modifications apportées par les mises à jour sont bien intégrées sans provoquer d\'erreurs.',
+                    'Identification et diagnostic des erreurs dans les fichiers PHP des thèmes.',
+                    'Correction des erreurs pour améliorer la fonctionnalité et la performance du site.',
+                    'Vérification des modifications pour assurer la compatibilité avec le reste du site et éviter toute régression.'
+                ],
+                'category' => 'Maintenance' ,
+                'image' => asset('img/ifs.jpeg'),        
             ],
         ];
 
--- resources/views/project/show.blade.php
+++ resources/views/project/show.blade.php
@@ -1,5 +1,46 @@
-    <div class="project-details">
-        <h1>{{ $project['name'] }}</h1>
-        <p>{{ $project['description'] }}</p>
-        <a href="{{ route('projects.index') }}">Retour à la liste des projets</a>
+@vite('resources/css/projects.css')
+@vite('resources/css/app.css')
+
+<body class="antialiased">
+    <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter selection:bg-red-500 selection:text-white">
+        @include('_nav')
+
+        <div class="max-w-7xl mx-auto p-6 lg:p-8">
+            <div class="mt-16">
+                <section>
+                    <h5 class="heading-primary-portfolio">{{ $project['category'] }}</h5>
+                    <h1 class="heading-secondary">{{ $project['subtitle'] }}</h1>
+                    @if(isset($project['image']))
+                        <img src="{{ asset($project['image']) }}" alt="{{ $project['name'] }}" class="project-image">
+                    @endif
+ 
+                    <div class="project-details">
+                        <h2 class="title-list">Description</h2>
+                        <div class="project-description">
+                            <p>{{ $project['description'] }}</p>
+                            @foreach($project['desc-list'] as $task)
+                                <p class="desc-list">{{ $task }}</p>
+                            @endforeach
+                        </div>
+                        <h2 class="title-list">Rôle</h2>
+                        <div class="project-role">
+                            <p>{{ $project['role'] }}</p>
+                        </div>
+                        <h2 class="title-list">Tâches</h2>
+                        <div class="project-taches">
+                            <ul>
+                                @foreach($project['liste'] as $task)
+                                    <li>{{ $task }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <a class="return-button" href="{{ route('projects.index') }}">Retour à la liste des projets</a>
+        </div>
     </div>
+</body>
+
+@include('_footer')
+@include('_sponsor')
