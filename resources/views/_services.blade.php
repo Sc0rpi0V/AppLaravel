42b5d00770b05a1cbbbc9fba3c18124c0e7bcefb
valentin <valentinc.boury@gmail.com>
2024-09-18 10:56:13 +0200

    Changement sur affichage des services page acceuil

--- resources/views/_services.blade.php
+++ resources/views/_services.blade.php
@@ -1,6 +1,6 @@
 <div class="mt-16">
     <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
-        <a class="scale-100 p-6 bg-violet-100 dark:bg-violet-800/50 dark:bg-gradient-to-bl from-violet-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-violet-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+        <a href="{{ route('services.conception') }}" class="scale-100 p-6 bg-violet-100 dark:bg-violet-800/50 dark:bg-gradient-to-bl from-violet-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-violet-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
             <div>
                 <div class="h-16 w-16 bg-white-50 dark:bg-violet-800/20 flex items-center justify-center rounded-full">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-7 h-7 stroke-white">
@@ -17,11 +17,11 @@
             </div>
         </a>
 
-        <a class="scale-100 p-6 bg-violet-100 dark:bg-violet-800/50 dark:bg-gradient-to-bl from-violet-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-violet-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+        <a href="{{ route('services.developpement') }}" class="scale-100 p-6 bg-violet-100 dark:bg-violet-800/50 dark:bg-gradient-to-bl from-violet-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-violet-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
             <div>
                 <div class="h-16 w-16 bg-violet-50 dark:bg-violet-800/20 flex items-center justify-center rounded-full">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-7 h-7 stroke-white">
-                        <path stroke-linecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z" />
                     </svg>
                 </div>
 
@@ -32,14 +32,13 @@
                     un dashboard 100% customisable.
                 </p>
             </div>
-
         </a>
 
-        <a class="scale-100 p-6 bg-violet-100 dark:bg-violet-800/50 dark:bg-gradient-to-bl from-violet-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-violet-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+        <a href=" {{ route('services.consultation') }} " class="scale-100 p-6 bg-violet-100 dark:bg-violet-800/50 dark:bg-gradient-to-bl from-violet-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-violet-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
             <div>
                 <div class="h-16 w-16 bg-violet-50 dark:bg-violet-800/20 flex items-center justify-center rounded-full">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-7 h-7 stroke-white">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.115 5.19l.319 1.913A6 6 0 008.11 10.36L9.75 12l-.387.775c-.217.433-.132.956.21 1.298l1.348 1.348c.21.21.329.497.329.795v1.089c0 .426.24.815.622 1.006l.153.076c.433.217.956.132 1.298-.21l.723-.723a8.7 8.7 0 002.288-4.042 1.087 1.087 0 00-.358-1.099l-1.33-1.108c-.251-.21-.582-.299-.905-.245l-1.17.195a1.125 1.125 0 01-.98-.314l-.295-.295a1.125 1.125 0 010-1.591l.13-.132a1.125 1.125 0 011.3-.21l.603.302a.809.809 0 001.086-1.086L14.25 7.5l1.256-.837a4.5 4.5 0 001.528-1.732l.146-.292M6.115 5.19A9 9 0 1017.18 4.64M6.115 5.19A8.965 8.965 0 0112 3c1.929 0 3.716.607 5.18 1.64" />
                     </svg>
                 </div>
 
@@ -49,24 +48,23 @@
                     Optimisation de la visibilité de votre site internet grâce à une stratégie de référencement naturel efficace.
                 </p>
             </div>
-
         </a>
 
-        <a class="scale-100 p-6 bg-violet-100 dark:bg-violet-800/50 dark:bg-gradient-to-bl from-violet-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-violet-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+        <a href=" {{route('services.support') }} " class="scale-100 p-6 bg-violet-100 dark:bg-violet-800/50 dark:bg-gradient-to-bl from-violet-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-violet-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
             <div>
                 <div class="h-16 w-16 bg-violet-50 dark:bg-violet-800/20 flex items-center justify-center rounded-full">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-7 h-7 stroke-white">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.115 5.19l.319 1.913A6 6 0 008.11 10.36L9.75 12l-.387.775c-.217.433-.132.956.21 1.298l1.348 1.348c.21.21.329.497.329.795v1.089c0 .426.24.815.622 1.006l.153.076c.433.217.956.132 1.298-.21l.723-.723a8.7 8.7 0 002.288-4.042 1.087 1.087 0 00-.358-1.099l-1.33-1.108c-.251-.21-.582-.299-.905-.245l-1.17.195a1.125 1.125 0 01-.98-.314l-.295-.295a1.125 1.125 0 010-1.591l.13-.132a1.125 1.125 0 011.3-.21l.603.302a.809.809 0 001.086-1.086L14.25 7.5l1.256-.837a4.5 4.5 0 001.528-1.732l.146-.292M6.115 5.19A9 9 0 1017.18 4.64M6.115 5.19A8.965 8.965 0 0112 3c1.929 0 3.716.607 5.18 1.64" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9.67L12 12.41m2.74-2.74a2.5 2.5 0 013.53 0l1.06 1.06a2.5 2.5 0 010 3.53l-4.13 4.13a2.5 2.5 0 01-3.53 0l-1.06-1.06a2.5 2.5 0 010-3.53m4.13 4.13L9.5 9.5m0 0L7.76 7.76a1.5 1.5 0 00-2.12 0L4.5 8.9a1.5 1.5 0 000 2.12l5.62 5.62a1.5 1.5 0 002.12 0l1.14-1.14a1.5 1.5 0 000-2.12L9.5 9.5z" />
                     </svg>
                 </div>
 
                 <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">TMA et Plugins</h2>
 
                 <p class="mt-4 text-white text-sm leading-relaxed">
-                    Réalisation de maintenance applicative sur votre thème, contenu et coeur de WordPress, ainsi que de la 
-                    réalisation de plugins sur mesure.
+                    Réalisation de maintenance applicative sur votre thème, contenu et cœur de WordPress, ainsi que la 
+                    création de plugins sur mesure pour répondre à vos besoins spécifiques.
                 </p>
             </div>
-        </a>  
+        </a>
     </div>
 </div>
